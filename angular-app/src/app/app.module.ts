3407f2c7b5e4e5c8431be7af64c12c6fc95e956d
Isidora <isidora.cvetkov96@gmail.com>
2019-07-28 21:05:03 +0200

    Navbar i Login komponente

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RegisterComponent } from './register/register.component';
+import { LogInComponent } from './log-in/log-in.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    RegisterComponent,
+    LogInComponent
   ],
   imports: [
     BrowserModule,
