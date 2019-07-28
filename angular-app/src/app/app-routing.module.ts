5088e01cdedb6094df89e05964f1ceb99eb02204
Isidora <isidora.cvetkov96@gmail.com>
2019-07-28 22:57:12 +0200

    Register komponenta

--- angular-app/src/app/app-routing.module.ts
+++ angular-app/src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RegisterComponent } from './register/register.component';
+import { LogInComponent } from './log-in/log-in.component';
 
-const routes: Routes = [];
+
+const routes : Routes = [
+  {path:"log-in", component: LogInComponent},
+  {path:"register", component: RegisterComponent},
+
+  // {path: "", component: HomeComponent, pathMatch: "full"},
+  // {path: "**", redirectTo: "home"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
