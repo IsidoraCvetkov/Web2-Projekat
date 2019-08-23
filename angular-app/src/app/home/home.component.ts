2848de2275e9a008829b973c58dd9430ea107fdd
Isidora <isidora.cvetkov96@gmail.com>
2019-08-23 13:10:03 +0200

    Uskladjivanje Home stranice za razlicite korisnike

--- angular-app/src/app/home/home.component.ts
+++ angular-app/src/app/home/home.component.ts
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  public isUnregistrated: boolean;
+  public isRegistrated: boolean;
+  public isAdmin: boolean;
+  public isController: boolean;
 
   constructor() { }
 
   ngOnInit() {
+    this.isUnregistrated=this.isRegistrated=this.isAdmin=this.isController=false;
+    if(localStorage.role == "Admin"){
+      this.isAdmin=true;
+    }else if(localStorage.role=="AppUser"){
+      this.isRegistrated=true;
+    }else if(localStorage.role=="Controller"){
+      this.isController=true;
+    }else{
+      this.isUnregistrated=true;
+    }
   }
 
 }
